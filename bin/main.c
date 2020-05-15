33b5296eca41d857c97c39ae1dd133935b808a04
Leandro <leandro@mytapp.com.br>
2020-05-15 15:35:49 -0300

    fix timeouts of reading data

--- bin/main.c
+++ bin/main.c
@@ -4,16 +4,16 @@
 #include "rs232.h"
 #include "dispenser.h"
 
-int SERIAL_PORT = 6;       //COM7
-int BAUDRATE    = 9600;
+int SERIAL_PORT = 6; //COM7
+int BAUDRATE = 9600;
 char LOG_FILE[50] = "log.txt";
 
 void log_action(char *str1, char *str2)
 {
     FILE *f = fopen(LOG_FILE, "a");
     time_t current_time;
-    char* c_time_string;
-    
+    char *c_time_string;
+
     /* Obtain current time. */
     current_time = time(NULL);
 
@@ -26,36 +26,36 @@ void log_action(char *str1, char *str2)
 
 uint8_t open_port()
 {
-    char mode[]={'8','N','1',0};
+    char mode[] = {'8', 'N', '1', 0};
     char str[30];
 
-    if(RS232_OpenComport(SERIAL_PORT, BAUDRATE, mode))
+    if (RS232_OpenComport(SERIAL_PORT, BAUDRATE, mode))
     {
         sprintf(str, "Try to open serial port %d", SERIAL_PORT);
         log_action(str, "Can not open serial port");
-        
+
         return 0;
     }
-  
+
     return 1;
 }
 
 // Code 1: Success
 void print_success()
 {
-    printf("1\n");  
+    printf("1\n");
 }
 
 // Code 0: Fail
 void print_failure()
 {
-    printf("0\n");  
+    printf("0\n");
 }
 
 // Code -1: Unable to connect
 void print_unavaliable()
 {
-    printf("-1\n");  
+    printf("-1\n");
 }
 
 void send_to_read_position()
@@ -66,21 +66,22 @@ void send_to_read_position()
     char str1[60], str2[50];
 
     int length, answer;
-    
+
     length = pack(data, 2, CMD_DISPENSER_SEND_CARD_TO_READ_POSITION); // send to pos 1
-    RS232_SendBuf(SERIAL_PORT, data, length);   
+    RS232_SendBuf(SERIAL_PORT, data, length);
     sprintf(str1, "Received command SEND_CARD_TO_READ from application");
-    sprintf(str2, "Sending: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", data[0],data[1],data[2],data[3],data[4],data[5],data[6]);
+    sprintf(str2, "Sending: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
     log_action(str1, str2);
-        
-    #ifdef _WIN32
-    Sleep(1000);    /* sleep for 1000 milliSeconds */
+
+#ifdef _WIN32
+    Sleep(1000); /* sleep for 1000 milliSeconds */
 #else
-    usleep(1000000);  
+    usleep(1000000);
 #endif
     answer = RS232_PollComport(SERIAL_PORT, buffer, 10);
-    
-    if(answer > 0 && buffer[0] != RETURN_NAK)
+    printf("answer %d", answer);
+    printf("answer %d", buffer[0]);
+    if (answer > 0 && buffer[0] != RETURN_NAK)
     {
         sprintf(str2, "0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6]);
         log_action("Received response from MT166", str2);
@@ -92,13 +93,13 @@ void send_to_read_position()
         return;
     }
 
-    if(buffer[5] == RETURN_OPERATION_SUCCEED)
+    if (buffer[5] == RETURN_OPERATION_SUCCEED)
     {
         print_success();
     }
     else
     {
-        print_failure();  
+        print_failure();
     }
 }
 
@@ -110,21 +111,20 @@ void send_to_dispense_position()
     char str1[60], str2[50];
 
     int length, answer;
-    
+
     length = pack(data, 2, CMD_DISPENSER_SEND_CARD_TO_DISPENSE_POSITION); // send to pos 2
-    RS232_SendBuf(SERIAL_PORT, data, length);   
+    RS232_SendBuf(SERIAL_PORT, data, length);
     sprintf(str1, "Received command DISPENSE_CARD from application");
-    sprintf(str2, "Sending: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", data[0],data[1],data[2],data[3],data[4],data[5],data[6]);
+    sprintf(str2, "Sending: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
     log_action(str1, str2);
-        
-    #ifdef _WIN32
-    Sleep(1000);    /* sleep for 1000 milliSeconds */
+
+#ifdef _WIN32
+    Sleep(1500); /* sleep for 1000 milliSeconds */
 #else
-    usleep(1000000);  
+    usleep(1000000);
 #endif
     answer = RS232_PollComport(SERIAL_PORT, buffer, 10);
-    
-    if(answer > 0 && buffer[0] != RETURN_NAK)
+    if (answer > 0 && buffer[0] != RETURN_NAK)
     {
         sprintf(str2, "0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6]);
         log_action("Received response from MT166", str2);
@@ -136,13 +136,13 @@ void send_to_dispense_position()
         return;
     }
 
-    if(buffer[5] == RETURN_OPERATION_SUCCEED)
+    if (buffer[5] == RETURN_OPERATION_SUCCEED)
     {
         print_success();
     }
     else
     {
-        print_failure();  
+        print_failure();
     }
 }
 
@@ -154,21 +154,21 @@ void check_card_at_dispense_position()
     char str1[70], str2[50];
 
     int length, answer;
-    
-    length = pack(data, 2, CMD_DISPENSER_CHECK_STATUS); 
+
+    length = pack(data, 2, CMD_DISPENSER_CHECK_STATUS);
     RS232_SendBuf(SERIAL_PORT, data, length);
     sprintf(str1, "Received command CHECK_CARD_AT_DISPENSE_POSITION from application");
-    sprintf(str2, "Sending: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", data[0],data[1],data[2],data[3],data[4],data[5],data[6]);
+    sprintf(str2, "Sending: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
     log_action(str1, str2);
-        
-    #ifdef _WIN32
-    Sleep(100);    /* sleep for 1000 milliSeconds */
+
+#ifdef _WIN32
+    Sleep(1000); /* sleep for 1000 milliSeconds */
 #else
-    usleep(100000);  
+    usleep(100000);
 #endif
     answer = RS232_PollComport(SERIAL_PORT, buffer, 10);
-    
-    if(answer > 0 && buffer[0] != RETURN_NAK)
+
+    if (answer > 0 && buffer[0] != RETURN_NAK)
     {
         sprintf(str2, "0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6]);
         log_action("Received response from MT166", str2);
@@ -180,13 +180,13 @@ void check_card_at_dispense_position()
         return;
     }
 
-    if(buffer[5] & DISPENSER_STATUS_CARD_AT_DISPENSE)
+    if (buffer[5] & DISPENSER_STATUS_CARD_AT_DISPENSE)
     {
         print_success();
     }
     else
     {
-        print_failure();  
+        print_failure();
     }
 }
 
@@ -198,21 +198,21 @@ void check_card_at_read_position()
     char str1[70], str2[50];
 
     int length, answer;
-    
-    length = pack(data, 2, CMD_DISPENSER_CHECK_STATUS); 
+
+    length = pack(data, 2, CMD_DISPENSER_CHECK_STATUS);
     RS232_SendBuf(SERIAL_PORT, data, length);
     sprintf(str1, "Received command CHECK_CARD_AT_READ_POSITION from application");
-    sprintf(str2, "Sending: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", data[0],data[1],data[2],data[3],data[4],data[5],data[6]);
+    sprintf(str2, "Sending: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
     log_action(str1, str2);
-        
-    #ifdef _WIN32
-    Sleep(100);    /* sleep for 1000 milliSeconds */
+
+#ifdef _WIN32
+    Sleep(1000); /* sleep for 1000 milliSeconds */
 #else
-    usleep(100000);  
+    usleep(100000);
 #endif
     answer = RS232_PollComport(SERIAL_PORT, buffer, 10);
-    
-    if(answer > 0 && buffer[0] != RETURN_NAK)
+
+    if (answer > 0 && buffer[0] != RETURN_NAK)
     {
         sprintf(str2, "0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6]);
         log_action("Received response from MT166", str2);
@@ -224,13 +224,13 @@ void check_card_at_read_position()
         return;
     }
 
-    if(buffer[5] & DISPENSER_STATUS_CARD_AT_READ)
+    if (buffer[5] & DISPENSER_STATUS_CARD_AT_READ)
     {
         print_success();
     }
     else
     {
-        print_failure();  
+        print_failure();
     }
 }
 
@@ -242,21 +242,21 @@ void check_if_box_is_pre_empty()
     char str1[70], str2[50];
 
     int length, answer;
-    
-    length = pack(data, 2, CMD_DISPENSER_CHECK_STATUS); 
+
+    length = pack(data, 2, CMD_DISPENSER_CHECK_STATUS);
     RS232_SendBuf(SERIAL_PORT, data, length);
     sprintf(str1, "Received command CHECK_IF_BOX_IS_ALMOST_EMPTY from application");
-    sprintf(str2, "Sending: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", data[0],data[1],data[2],data[3],data[4],data[5],data[6]);
+    sprintf(str2, "Sending: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
     log_action(str1, str2);
-        
-    #ifdef _WIN32
-    Sleep(100);    /* sleep for 1000 milliSeconds */
+
+#ifdef _WIN32
+    Sleep(1000); /* sleep for 1000 milliSeconds */
 #else
-    usleep(100000);  
+    usleep(100000);
 #endif
     answer = RS232_PollComport(SERIAL_PORT, buffer, 10);
-    
-    if(answer > 0 && buffer[0] != RETURN_NAK)
+
+    if (answer > 0 && buffer[0] != RETURN_NAK)
     {
         sprintf(str2, "0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6]);
         log_action("Received response from MT166", str2);
@@ -268,13 +268,13 @@ void check_if_box_is_pre_empty()
         return;
     }
 
-    if(buffer[5] & DISPENSER_STATUS_CARD_SHORTAGE)
+    if (buffer[5] & DISPENSER_STATUS_CARD_SHORTAGE)
     {
         print_success();
     }
     else
     {
-        print_failure();  
+        print_failure();
     }
 }
 
@@ -286,21 +286,21 @@ void check_if_box_is_empty()
     char str1[70], str2[50];
 
     int length, answer;
-    
-    length = pack(data, 2, CMD_DISPENSER_CHECK_STATUS); 
+
+    length = pack(data, 2, CMD_DISPENSER_CHECK_STATUS);
     RS232_SendBuf(SERIAL_PORT, data, length);
     sprintf(str1, "Received command CHECK_IF_BOX_IS_EMPTY from application");
-    sprintf(str2, "Sending: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", data[0],data[1],data[2],data[3],data[4],data[5],data[6]);
+    sprintf(str2, "Sending: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
     log_action(str1, str2);
-        
-    #ifdef _WIN32
-    Sleep(100);    /* sleep for 1000 milliSeconds */
+
+#ifdef _WIN32
+    Sleep(1000); /* sleep for 1000 milliSeconds */
 #else
-    usleep(100000);  
+    usleep(100000);
 #endif
     answer = RS232_PollComport(SERIAL_PORT, buffer, 10);
-    
-    if(answer > 0 && buffer[0] != RETURN_NAK)
+
+    if (answer > 0 && buffer[0] != RETURN_NAK)
     {
         sprintf(str2, "0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6]);
         log_action("Received response from MT166", str2);
@@ -312,13 +312,13 @@ void check_if_box_is_empty()
         return;
     }
 
-    if(buffer[5] & DISPENSER_STATUS_BOX_EMPTY)
+    if (buffer[5] & DISPENSER_STATUS_BOX_EMPTY)
     {
-        print_success();  
+        print_success();
     }
     else
     {
-        print_failure();  
+        print_failure();
     }
 }
 
@@ -330,21 +330,21 @@ void discard_card()
     char str1[60], str2[50];
 
     int length, answer;
-    
+
     length = pack(data, 2, CMD_DISPENSER_ACCEPT_CARD_TO_RECYCLE); // send to recycle
-    RS232_SendBuf(SERIAL_PORT, data, length);   
+    RS232_SendBuf(SERIAL_PORT, data, length);
     sprintf(str1, "Received command DISCARD_CARD from application");
-    sprintf(str2, "Sending: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", data[0],data[1],data[2],data[3],data[4],data[5],data[6]);
+    sprintf(str2, "Sending: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
     log_action(str1, str2);
-        
-    #ifdef _WIN32
-    Sleep(2000);    /* sleep for 1000 milliSeconds */
+
+#ifdef _WIN32
+    Sleep(2000); /* sleep for 1000 milliSeconds */
 #else
-    usleep(2000000);  
+    usleep(2000000);
 #endif
     answer = RS232_PollComport(SERIAL_PORT, buffer, 10);
-    
-    if(answer > 0 && buffer[0] != RETURN_NAK)
+
+    if (answer > 0 && buffer[0] != RETURN_NAK)
     {
         sprintf(str2, "0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6]);
         log_action("Received response from MT166", str2);
@@ -356,13 +356,13 @@ void discard_card()
         return;
     }
 
-    if(buffer[5] == RETURN_OPERATION_SUCCEED)
+    if (buffer[5] == RETURN_OPERATION_SUCCEED)
     {
         print_success();
     }
     else
     {
-        print_failure();  
+        print_failure();
     }
 }
 
@@ -374,21 +374,21 @@ void get_dispenser_version()
     char str1[60], str2[50];
 
     int length, answer;
-    
+
     length = pack(data, 2, CMD_DISPENSER_CHECK_SOFTWARE_VERSION); // send to recycle
-    RS232_SendBuf(SERIAL_PORT, data, length);   
+    RS232_SendBuf(SERIAL_PORT, data, length);
     sprintf(str1, "Received command CHECK_VERSION from application");
-    sprintf(str2, "Sending: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", data[0],data[1],data[2],data[3],data[4],data[5],data[6]);
+    sprintf(str2, "Sending: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
     log_action(str1, str2);
-        
-    #ifdef _WIN32
-    Sleep(2000);    /* sleep for 1000 milliSeconds */
+
+#ifdef _WIN32
+    Sleep(2000); /* sleep for 1000 milliSeconds */
 #else
-    usleep(2000000);  
+    usleep(2000000);
 #endif
     answer = RS232_PollComport(SERIAL_PORT, buffer, 10);
-    
-    if(answer > 0 && buffer[0] != RETURN_NAK)
+
+    if (answer > 0 && buffer[0] != RETURN_NAK)
     {
         sprintf(str2, "0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x", buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6]);
         log_action("Received response from MT166", str2);
@@ -403,27 +403,36 @@ void get_dispenser_version()
 
 void menu(int command)
 {
-    switch(command)
+    switch (command)
     {
-        case 1: send_to_read_position();
+    case 1:
+        send_to_read_position();
         break;
-        case 2: send_to_dispense_position();
+    case 2:
+        send_to_dispense_position();
         break;
-        case 3: check_card_at_dispense_position();
+    case 3:
+        check_card_at_dispense_position();
         break;
-        case 4: check_card_at_read_position();
+    case 4:
+        check_card_at_read_position();
         break;
-        case 5: check_if_box_is_pre_empty();
+    case 5:
+        check_if_box_is_pre_empty();
         break;
-        case 6: check_if_box_is_empty();
+    case 6:
+        check_if_box_is_empty();
         break;
-        case 7: discard_card();
+    case 7:
+        discard_card();
         break;
-        case 8: get_dispenser_version();
+    case 8:
+        get_dispenser_version();
         break;
 
-        default: printf("Invalid parameter \n");           
-    }    
+    default:
+        printf("Invalid parameter \n");
+    }
 }
 
 void close_port()
@@ -433,26 +442,26 @@ void close_port()
 
 int main(int argc, char **argv)
 {
-    if(argc < 2)
+    if (argc < 2)
     {
         printf("%s: missing operand.\nUsage: %s <command> <serial port(optional)> <baudrate(optional)> <log file path(optional)> \n", argv[0], argv[0]);
         exit(0);
     }
 
-    if(argc >= 3)
+    if (argc >= 3)
     {
-        SERIAL_PORT = atoi(argv[2]); 
+        SERIAL_PORT = atoi(argv[2]);
     }
-    if(argc >= 4)
+    if (argc >= 4)
     {
-        BAUDRATE    = atoi(argv[3]);
+        BAUDRATE = atoi(argv[3]);
     }
-    if(argc >= 5)
+    if (argc >= 5)
     {
         strcpy(LOG_FILE, argv[5]);
     }
 
-    if(open_port())
+    if (open_port())
     {
         menu(atoi(argv[1]));
 
@@ -460,4 +469,3 @@ int main(int argc, char **argv)
     }
     return 0;
 }
-
